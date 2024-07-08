4d3c6da2b47331cacef67e39827a01a5938dc47d
rbolanosa <rbolanosa17_1@unc.edu.pe>
2024-07-08 11:12:42 -0500

    ultimo

---
+++ js/script1.js
@@ -0,0 +1,8 @@
+document.getElementById('institutional-email').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('parent-access').addEventListener('click', function() {
+    // Aquí puedes redirigir a la página de acceso a padres de familia
+    // window.location.href = 'login-parents.html';
+});
