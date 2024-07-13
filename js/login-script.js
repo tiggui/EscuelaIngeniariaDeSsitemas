cf3410101fd7f180da1dd9546f26f071e22a0905
rbolanosa <rbolanosa17_1@unc.edu.pe>
2024-07-13 12:27:12 -0500

    aaa

--- js/login-script.js
+++ js/login-script.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     // Validar email y contraseña (ejemplo simple)
     if (email === 'rbolanosa17_1@unc.edu.pe' && password === '123456789') {
-        window.location.href = 'index.html'; // Redirigir a index.html
+        window.location.href = 'Main.html'; // Redirigir a Main.html
     } else {
         alert('Correo o contraseña incorrectos');
     }
