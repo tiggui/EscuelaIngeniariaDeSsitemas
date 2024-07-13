36e6636635fa2ba45264d47ffce4c126e9550bdb
tiggui <acaruajulcat22_2@unc.edu.pe>
2024-07-13 15:09:37 -0500

    conocenos

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ document.getElementById('parent-access').addEventListener('click', function() {
     // Aquí puedes redirigir a la página de acceso a padres de familia
     // window.location.href = 'login-parents.html';
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var acc = document.getElementsByClassName('accordion-header');
+    
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+            var content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    }
+});
+
+function hola(){
+    var acc = document.getElementsByClassName('accordion-header');
+    
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+            var content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    }
+}
